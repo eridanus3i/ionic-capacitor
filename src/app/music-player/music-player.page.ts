0a5aaab04a052ef08c9032f185c3caed6487a3f3
Jake Hu <hotranphihung@gmail.com>
2024-02-19 16:27:49 +0700

    s

--- src/app/music-player/music-player.page.ts
+++ src/app/music-player/music-player.page.ts
@@ -22,23 +22,53 @@ export class MusicPlayerPage implements OnInit {
 
   playlist: Track[] = [
     {
-      name: "Jazzy Sounds",
-      path: "./assets/mp3/jazzy-sounds.mp3",
+      name: "Chapter 1",
+      path: "./assets/sounds/chapter1.mp3",
     },
     {
-      name: "Footage Feet",
-      path: "./assets/mp3/footage-feet.mp3",
+      name: "Chapter 2",
+      path: "./assets/sounds/chapter2.mp3",
     },
     {
-      name: "Death Grips",
-      path: "./assets/mp3/death-grips.mp3",
+      name: "Chapter 3",
+      path: "./assets/sounds/chapter3.mp3",
     },
+    {
+      name: "Chapter 4",
+      path: "./assets/sounds/chapter4.mp3",
+    },
+    {
+      name: "Chapter 5",
+      path: "./assets/sounds/chapter5.mp3",
+    },
+    {
+      name: "Chapter 6",
+      path: "./assets/sounds/chapter6.mp3",
+    },
+    {
+      name: "Chapter 7",
+      path: "./assets/sounds/chapter7.mp3",
+    },
+    {
+      name: "Chapter 8",
+      path: "./assets/sounds/chapter8.mp3",
+    },
+    {
+      name: "Chapter 9",
+      path: "./assets/sounds/chapter9.mp3",
+    },
+    {
+      name: "Chapter 10",
+      path: "./assets/sounds/chapter10.mp3",
+    }
   ];
 
   activeTrack: Track = { name: "", path: "" };
   player: Howl | undefined;
   isPlaying = false;
   progress = 0;
+  currentTime = "0:00"; // Current playback time
+
   @ViewChild("range", { static: false }) range!: IonRange;
 
 
@@ -58,6 +88,7 @@ export class MusicPlayerPage implements OnInit {
       onend: () => {
         console.log("onend");
         this.isPlaying = false;
+        this.currentTime = "0:00"; // Reset current time when playback ends
       },
     });
     this.player.play();
@@ -93,9 +124,16 @@ export class MusicPlayerPage implements OnInit {
     if (!this.player) return;
     const seek = this.player.seek();
     this.progress = (seek / this.player.duration()) * 100 || 0;
+    this.currentTime = this.formatTime(Math.round(seek)); // Format current time
     if (this.isPlaying) {
       requestAnimationFrame(() => this.updateProgress());
     }
+  }
+  formatTime(seconds: number) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const displaySeconds = remainderSeconds < 10 ? `0${remainderSeconds}` : remainderSeconds;
+    return `${minutes}:${displaySeconds}`;
   }
 
 }
