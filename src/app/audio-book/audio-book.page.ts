089600d66e9ea43c3c9c7551bb159acbbb6863fb
Jake Hu <hotranphihung@gmail.com>
2024-02-19 14:31:51 +0700

    s

--- src/app/audio-book/audio-book.page.ts
+++ src/app/audio-book/audio-book.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SpeechRecognition } from '@capacitor-community/speech-recognition';
 import { Filesystem, Directory } from '@capacitor/filesystem';
-import {NativeAudio} from '@capacitor-community/native-audio'
+import { NativeAudio } from '@capacitor-community/native-audio'
 
 
 @Component({
@@ -10,15 +10,17 @@ import {NativeAudio} from '@capacitor-community/native-audio'
   styleUrls: ['./audio-book.page.scss'],
 })
 export class AudioBookPage implements OnInit {
+  audioList: any[] | undefined 
 
-  constructor() { 
+  constructor() {
   }
 
-  async preloadAudio() {
+
+  async preloadAudio(audio: any) {
     try {
       await NativeAudio.preload({
         assetId: 'fire',
-        assetPath: 'chapter1.mp3', // Adjust the path as per your project structure
+        assetPath: audio.path, // Adjust the path as per your project structure
         audioChannelNum: 1,
         isUrl: false
       });
@@ -30,12 +32,54 @@ export class AudioBookPage implements OnInit {
 
 
   ngOnInit() {
-    SpeechRecognition.available().then((result) => {
-      console.log('SpeechRecognition available:', result.available);
-    });
+    this.audioList = [
+      {
+        name: 'Chapter 1',
+        path: 'chapter1.mp3'
+      },
+      {
+        name: 'Chapter 2',
+        path: 'chapter2.mp3'
+      },
+      {
+        name: 'Chapter 3',
+        path: 'chapter3.mp3'
+      },
+      {
+        name: 'Chapter 4',
+        path: 'chapter4.mp3'
+      },
+      {
+        name: 'Chapter 5',
+        path: 'chapter5.mp3'
+      },
+      {
+        name: 'Chapter 6',
+        path: 'chapter6.mp3'
+      },
+      {
+        name: 'Chapter 7',
+        path: 'chapter7.mp3'
+      },
+      {
+        name: 'Chapter 8',
+        path: 'chapter8.mp3'
+      },
+      {
+        name: 'Chapter 9',
+        path: 'chapter9.mp3'
+      },
+      {
+        name: 'Chapter 10',
+        path: 'chapter10.mp3'
+      }
+    ]
+    
   }
 
-  async playAudio() {
+  async playAudio(audio: any) {
+    await this.unloadAudio(audio);
+    await this.preloadAudio(audio);
     try {
       await NativeAudio.play({ assetId: 'fire' });
       console.log('Audio played successfully');
@@ -44,7 +88,7 @@ export class AudioBookPage implements OnInit {
     }
   }
 
-  async stopAudio() {
+  async stopAudio(audio: any) {
     try {
       await NativeAudio.stop({ assetId: 'fire' });
       console.log('Audio stopped successfully');
@@ -53,7 +97,7 @@ export class AudioBookPage implements OnInit {
     }
   }
 
-  async unloadAudio() {
+  async unloadAudio(audio: any) {
     try {
       await NativeAudio.unload({ assetId: 'fire' });
       console.log('Audio unloaded successfully');
